d41aa6d30ab6f236d6f3c79650373dafa71f80ba
weirdSugar <bluebirdy@163.com>
2019-09-27 13:37:09 +0800

    devtool

--- neteaseMusic/config/webpack.dev.conf.js
+++ neteaseMusic/config/webpack.dev.conf.js
@@ -1,32 +1,32 @@
-const baseWebpackConfig=require('./webpack.base.conf');
-const merge=require('webpack-merge');
-const HtmlWebpackPlugin=require('html-webpack-plugin');
-const webpack=require('webpack');
-const path=require('path')
-module.exports=merge(baseWebpackConfig,{
-  mode:'development',
-  output:{
-    filename:'js/[name].[hash:16].js',
+const baseWebpackConfig = require("./webpack.base.conf");
+const merge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+module.exports = merge(baseWebpackConfig, {
+  mode: "development",
+  devtool: "eval-source-map",
+  output: {
+    filename: "js/[name].[hash:16].js"
     // publicPath:'/'
   },
-  plugins:[
+  plugins: [
     new HtmlWebpackPlugin({
-      template:'public/index.html',
-      inject:'body',
-      minify:{
-        html5:true,
+      template: "public/index.html",
+      inject: "body",
+      minify: {
+        html5: true,
         removeComments: true,
         collapseWhitespace: false
-      },
-      
-    }),
+      }
+    })
     // new webpack.HotModuleReplacementPlugin()
   ],
-  devServer:{
-    port:'1122',
-    compress:true,
-    historyApiFallback:true,
-    hotOnly:true,
-    open:true,
+  devServer: {
+    port: "1122",
+    compress: true,
+    historyApiFallback: true,
+    hotOnly: true,
+    open: true
   }
-})
+});
